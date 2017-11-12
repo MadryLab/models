9ced7571449aae17886e3f6e9c9ea04d9ce878af
Logan Engstrom <engstrom@csail.mit.edu>
2017-11-12 01:01:47 -0500

    added

--- research/slim/process_data.sh
+++ research/slim/process_data.sh
@@ -3,15 +3,16 @@ set -e
 
 echo "imagenet code: \"8bG]%4\"\`-k\""
 export HOME=/scratch/engstrom/dummy_home
-mkdir $HOME
+mkdir -p $HOME
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
 DATA_DIR=/scratch/engstrom/imagenet-data
 
 rm -rf $DATA_DIR
-mkdir $DATA_DIR
+mkdir -p $DATA_DIR
 
-cd ../
-bazel build slim/download_and_convert_imagenet
+bazel clean
+
+bazel build download_and_convert_imagenet
 
 # run it
-bazel-bin/slim/download_and_convert_imagenet "${DATA_DIR}"
+bazel-bin/download_and_convert_imagenet "${DATA_DIR}"
